98d67fd8fab4bf16d5dc01d1fbd55e206290c548
Bastian de Byl <bastian.de.byl@oracle.com>
2020-10-12 19:34:06 -0400

    updated yubiswitch.sh

--- u2f/yubiswitch.sh
+++ u2f/yubiswitch.sh
@@ -12,7 +12,12 @@ OPTIONS
     -t, --toggle        Toggles the Yubikey enabled state
     -d, --disable       Disables the Yubikey device
     -e, --enable        Enables the Yubikey device
+    -n, --notify        Shows a notify message (requires libnotify)
 EOF
+
+enable=0
+toggle=0
+notify=0
 
 die() {
     printf '%s\n' "$1" >&2
@@ -35,7 +40,7 @@ fi
 
 get_yubikey_id() {
     printf '%s' \
-      "$(xinput --list | perl -lne 'print $1 if /[Yy]ubikey.*id=(\d+)/')"
+           "$(xinput --list | perl -lne 'print $1 if /[Yy]ubikey.*id=(\d+)/')"
 }
 
 get_yubikey_state() {
@@ -45,10 +50,23 @@ get_yubikey_state() {
 
 set_yubikey() {
     if [ -z "$1" ]; then
-       die 'ERROR: no value provided to set_yubikey'
+        die 'ERROR: no value provided to set_yubikey'
     fi
 
     xinput --set-prop "$(get_yubikey_id)" 'Device Enabled' "$1"
+    if [ "$notify" -eq 1 ]; then
+        case "$1" in
+            1)
+                msg="Enabled"
+                ;;
+            0)
+                msg="Disabled"
+                ;;
+            *)
+                ;;
+        esac
+        notify-send -u low "YubiKey" "$msg"
+    fi
 }
 
 toggle_yubikey() {
@@ -65,26 +83,45 @@ toggle_yubikey() {
     esac
 }
 
-case $1 in
-    -h|-\?|--help)
-        show_help
-        ;;
-    -e|--enable)
-        printf 'Enabling Yubikey\n'
-        set_yubikey '1'
-        ;;
-    -d|--disable)
-        printf 'Disabling Yubikey\n'
-        set_yubikey '0'
-        ;;
-    -t|--toggle)
-        printf 'Toggling Yubikey\n'
-        toggle_yubikey
-        ;;
-    -?*)
-        printf 'ERROR: Unknown option (ignored): %s\n' "$1" >&2
-        ;;
-    *)
-        show_help
-        ;;
-esac
+while :; do
+      case $1 in
+          -h|-\?|--help)
+              show_help
+              exit
+              ;;
+          -e|--enable)
+              printf 'Enabling Yubikey\n'
+              enable=1
+              ;;
+          -d|--disable)
+              printf 'Disabling Yubikey\n'
+              enable=0
+              ;;
+          -t|--toggle)
+              printf 'Toggling Yubikey\n'
+              toggle=1
+              ;;
+          -n|--notify)
+              notify=1
+              ;;
+          --)
+              shift
+              break
+              ;;
+          -?*)
+              printf 'ERROR: Unknown option (ignored): %s\n' "$1" >&2
+              ;;
+          *)
+              break
+              ;;
+      esac
+
+      shift
+done
+
+
+if [ "$toggle" -eq 1 ]; then
+    toggle_yubikey;
+else
+    set_yubikey "$enable"
+fi
